4caab514e6c4f5e88d465066069c00b178556c3c
GizmoBytes <94705828+ZinedineAouda@users.noreply.github.com>
2026-04-10 14:51:42 +0100

    Update admin.tsx

--- client/src/pages/admin.tsx
+++ client/src/pages/admin.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import type { Slave } from "@shared/schema";
-import { apiRequest, apiUrl } from "@/lib/queryClient";
+import { apiRequest, apiUrl, getQueryFn } from "@/lib/queryClient";
 import { useToast } from "@/hooks/use-toast";
 import { useWebsocket } from "@/hooks/use-websocket";
 import { Shield, RefreshCw, LogIn, LogOut, Wifi, Trash2, Pencil, BellOff, Volume2, VolumeX } from "lucide-react";
